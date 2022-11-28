21d72f33ecd0488c88fdbce730fd4adea60df637
gazizkv <valitovgaziz@gmail.com>
2022-11-28 16:24:13 +0500

    Navigate ListFragment.kt

--- app/src/main/java/com/example/odinhome/list/ListFragment.kt
+++ app/src/main/java/com/example/odinhome/list/ListFragment.kt
@@ -5,24 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.example.odinhome.R
+import com.example.odinhome.adapters.InfoCardListAdapter
 import com.example.odinhome.databinding.FragmentListBinding
 
 
 class listFragment : Fragment() {
 
     lateinit var binding: FragmentListBinding
+    lateinit var viewModel: ListFragmentViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        binding = FragmentListBinding.inflate(inflater)
+        viewModel = ViewModelProvider(requireActivity())[ListFragmentViewModel::class.java]
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = InfoCardListAdapter()
+        binding.entitiyList.adapter = adapter
+
+        viewModel.list.observe(requireActivity()) {
+            adapter.items = it
+        }
+
+        binding.addToList.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_listFragment_to_editFragment)
+        }
     }
 
 
